879c9dc2ffc1cd26e9f905f8d0a7a3a9ae974577
Ak-Yadav3122 <aichchhik12@gmail.com>
2025-03-27 19:13:14 +0530

    create the QueryClient instance and QueryClientProvider

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { MainLayout } from "./components/Layout/MainLayout";
+import { Home } from "./Pages/Home";
+
+import "./App.css";
+
+// Create a router
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainLayout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+    ],
+  },
+]);
 
 const App = () => {
-  return (
-    <div>Hello Jee</div>
-  )
-}
+  const queryClient = new QueryClient();
 
-export default App
+  return (
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router}></RouterProvider>
+    </QueryClientProvider>
+  );
+};
+
+export default App;
